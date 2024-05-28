82cd50d16ca9c647136bfe2b01032bbc4bccf12c
Matthew Huebert <mhuebert@gmail.com>
2024-05-28 11:41:40 +0200

    org permissions: remove admins from lists

---
+++ scripts/org_permission_transition_data.py
@@ -0,0 +1,42 @@
+# %%
+
+# These are people who are org members (but not admins) who have
+# contributed to a repo in the past, and would lose access when 
+# the org base permissions are changed.
+
+implicit_collaborators_without_admins = {
+    "probcomp/Gen.jl": {
+        "arijitnoobstar",
+        "georgematheos",
+        "limarta",
+        "mirkoklukas",
+        "mugamma",
+        "postylem",
+        "sharlaon",
+    },
+    "probcomp/gen-quickstart": {"horizon-blue"},
+    "probcomp/gnlp": {"mugamma"},
+    "probcomp/InverseGraphicsRegistry": {"limarta"},
+    "probcomp/genjax": {"Joaoloula", "georgematheos", "limarta", "mugamma"},
+    "probcomp/GenSP.jl": {"mugamma"},
+    "probcomp/bayes3d": {"arijitnoobstar", "horizon-blue", "karen-sy"},
+    "probcomp/6412-probabilistic-fetch": {"arijitnoobstar"},
+    "probcomp/aistats2023-smcp3": {"mugamma"},
+    "probcomp/68410-monte-carlo-geometry-processing": {"mugamma"},
+    "probcomp/genjax-tuning": {"limarta"},
+    "probcomp/LLaMPPL": {"postylem"},
+    "probcomp/limarta-meng-thesis": {"limarta"},
+    "probcomp/genjax-gi": {"arijitnoobstar"},
+    "probcomp/welcome": {"MathieuHuot", "littleredcomputer", "mhuebert"},
+    "probcomp/PLDI23GenSPInverseGraphicsExperiment": {"karen-sy"},
+    "probcomp/hfppl": {"postylem"},
+    "probcomp/dcolmap": {"esli999", "georgematheos", "karen-sy", "mugamma"},
+    "probcomp/probcomp-localization-tutorial": {"limarta"},
+    "probcomp/Biophysical_SNMC": {"wsphillips"},
+    "probcomp/pldi24-iql-tex": {"MathieuHuot"},
+    "probcomp/icra-bayes3d": {"karen-sy", "mugamma"},
+    "probcomp/lafi24-snpl": {"georgematheos"},
+    "probcomp/whatsbehind": {"wsphillips"},
+    "probcomp/b3d": {"esli999", "georgematheos", "horizon-blue", "karen-sy"},
+    "probcomp/programmable-vi-pldi-2024": {"horizon-blue"},
+}
